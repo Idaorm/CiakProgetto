0dfea0d4d02fe2df86a54154dea579a8071cebe9
idaor <i.ormando@studenti.unisa.it>
2026-01-29 16:28:18 +0100

    Controllo Recensione

--- src/main/java/controller/util/DBConnection.java
+++ src/main/java/controller/util/DBConnection.java
@@ -1,11 +1,11 @@
-package util;
+package controller.util;
 
 import java.sql.*;
 
 public class DBConnection {
     private static final String URL = "jdbc:mysql://localhost:3306/CiakProgetto?useSSL=false&serverTimezone=UTC&allowPublicKeyRetrieval=true";
     private static final String USER = "root";
-    private static final String PASSWORD = "sofia";
+    private static final String PASSWORD = "idaa";
 
     public static Connection getConnection() throws SQLException {
         try {
